a63848d25656e94b29e2cfe9b84ea6fbe5287ca5
Ivan Petrov <petrovodt@gmail.com>
2020-04-05 17:45:27 +0300

    compare + sprinkle + bug fixes

--- src/main/java/Box.java
+++ src/main/java/Box.java
@@ -1,16 +1,39 @@
 import lesson_1.Fruits.Fruit;
+import org.w3c.dom.ls.LSOutput;
 
 import java.util.ArrayList;
 
 public class Box <T extends Fruit> {
-    private ArrayList<T> box = new ArrayList<T>();
+    public ArrayList<T> box = new ArrayList<T>();
 
     public void putInBox(T obj){
         box.add(obj);
     }
 
     public double getWeight(){
-        double weight = box.size() * T.getWeight();
+        double weight = 0.0f;
+        for (T fruit : box) {
+            weight += fruit.getWeight();
+        }
         return weight;
+    }
+
+    public void compare(Box boxy){
+        if(this.getWeight() == boxy.getWeight()){
+            System.out.println("equal");
+        }
+        if(this.getWeight() > boxy.getWeight() ){
+            System.out.println("current box is heavier");
+        }
+        if(this.getWeight() < boxy.getWeight()){
+            System.out.println("second box is heavier");
+        }
+    }
+
+    public void sprinkle(Box<T> boxToEmpty){
+        for (T item : boxToEmpty.box) {
+            this.box.add(item);
+        }
+        boxToEmpty.box.removeAll(boxToEmpty.box);
     }
 }
--- src/main/java/BoxTest.java
+++ src/main/java/BoxTest.java
@@ -5,14 +5,32 @@ public class BoxTest {
     public static void main(String[] args) {
         Apple a1 = new Apple();
         Apple a2 = new Apple();
+        Apple a3 = new Apple();
+        Orange o1 = new Orange();
+        Orange o2 = new Orange();
 
         Box<Apple> appleBox = new Box<>();
+        Box<Apple> secondAppleBox = new Box<>();
+        Box<Orange> orangeBox = new Box<>();
+
 
         appleBox.putInBox(a1);
         appleBox.putInBox(a2);
+        secondAppleBox.putInBox(a3);
+        orangeBox.putInBox(o1);
+        orangeBox.putInBox(o2);
 
-        Box<Orange> orangeBox = new Box<>();
-
+        System.out.println("Apple boxes weight: ");
         System.out.println(appleBox.getWeight());
+        System.out.println(secondAppleBox.getWeight());
+        System.out.println("Orange box weight: ");
+        System.out.println(orangeBox.getWeight());
+
+        System.out.println("Comparing first and second apple boxes: ");
+        appleBox.compare(orangeBox);
+
+        
+        secondAppleBox.sprinkle(appleBox);
+
     }
 }
