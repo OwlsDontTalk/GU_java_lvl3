9c6453f9b14b9764997525ffd31d8a63d25ec1de
Ivan Petrov <petrovodt@gmail.com>
2020-04-06 22:27:17 +0300

    2nd lesson init

--- src/main/java/BoxTest.java
+++ src/main/java/BoxTest.java
@@ -1,6 +1,8 @@
 import lesson_1.Fruits.Apple;
 import lesson_1.Fruits.Orange;
 
+import java.util.ArrayList;
+
 public class BoxTest {
     public static void main(String[] args) {
         Apple a1 = new Apple();
@@ -32,5 +34,33 @@ public class BoxTest {
         
         secondAppleBox.sprinkle(appleBox);
 
+       // itemSwap();
+        arrayToArrayList();
     }
+
+    private static void itemSwap(){
+        int[] s = {1,2,3,4,5,6,7};
+
+        int tmp = s[0];
+        s[0] = s[1];
+        s[1] = tmp;
+
+        for (int digit : s) {
+            System.out.println(digit);
+        }
+    }
+
+    private static void arrayToArrayList(){
+        int[] s = {1,2,3,4,5,6,7};
+        ArrayList<Integer> newList = new ArrayList<Integer>();
+
+        for (int i : s) {
+            newList.add(i);
+        }
+
+        for (int i: newList) {
+            System.out.println(i);
+        }
+    }
+
 }
