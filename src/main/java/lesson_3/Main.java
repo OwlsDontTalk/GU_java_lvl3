133998d882d9efb7059db17fbb8ca41856f5f1a7
Ivan Petrov <petrovodt@gmail.com>
2020-04-12 15:33:36 +0300

    task 3 solution

--- src/main/java/lesson_3/Main.java
+++ src/main/java/lesson_3/Main.java
@@ -3,7 +3,9 @@ package lesson_3;
 //+ 1.Прочитать файл (около 50 байт) в байтовый массив и вывести этот массив в консоль;
 //+2. Последовательно сшить 5 файлов в один (файлы примерно 100 байт).
 //+3. Может пригодиться следующая конструкция: ArrayList<InputStream> al = new ArrayList<>(); ... Enumeration<InputStream> e = Collections.enumeration(al);
-//4. Написать консольное приложение, которое умеет постранично читать текстовые файлы (размером > 10 mb). Вводим страницу (за страницу можно принять 1800 символов), программа выводит ее в консоль. Контролируем время выполнения: программа не должна загружаться дольше 10 секунд, а чтение – занимать свыше 5 секунд.
+//4. Написать консольное приложение, которое умеет постранично читать текстовые файлы (размером > 10 mb).
+// Вводим страницу (за страницу можно принять 1800 символов), программа выводит ее в консоль.
+// Контролируем время выполнения: программа не должна загружаться дольше 10 секунд, а чтение – занимать свыше 5 секунд.
 //5. Сделать клиен-серверное приложение. Передать по сети сеарилизованный объект.
 
 import java.awt.*;
@@ -14,8 +16,9 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        readFile();
-        readFiewFiles   ();
+        //readFile();
+        //readFiewFiles();
+        readBigFile();
     }
 
     public static void readFile(){
@@ -59,5 +62,19 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void readBigFile() throws IOException {
+        long t = System.currentTimeMillis();
+        int page = 0;
+        Reader reader = new InputStreamReader(new FileInputStream("lesson-3/task3-loremIpsum.txt"));
+        char[] chars = new char[1800];
+        while (reader.read(chars) != -1){
+            System.out.println("Page: " + page++);
+            System.out.println(chars);
+        }
+
+        System.out.println("\n\nTotal pages printed: " + page);
+        System.out.println("Time spent: " + (System.currentTimeMillis() - t));
     }
 }
